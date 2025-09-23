5f79b52702402930ef84d36f0ec8882798e09afa
GhuleAjinkya <ghule.ajinkya123@gmail.com>
2025-09-23 08:43:21 +0530

    gitignore changes

--- demo.py
+++ demo.py
@@ -6,6 +6,8 @@ def simulateTransfer():
     cursor.execute("use bank;")
     try:
         cursor.callproc("TransferAmount",[1,2,14000,1])
+        # call TransferAmount(3,4,100.00,0);
+        # call TransferAmount(1,2,14000.00,0);
     except Exception as e:
         print("error: ", e)
      
