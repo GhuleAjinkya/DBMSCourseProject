72f1374ec9df7d1c506f4ffecc2abfc997b95b91
GhuleAjinkya <ghule.ajinkya123@gmail.com>
2025-10-14 00:56:56 +0530

    added global settings changes

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import mysql.connector, logging, datetime
 from SetupCommands.Tables import Tables
 from SetupCommands.Data import Data
 from SetupCommands.Procedures import Procedures
+from SetupCommands.Globals import Globals
 import demo
 logger = logging.getLogger(__name__)
 
@@ -37,15 +38,21 @@ def databaseSetup():
         for procedure in Procedures:
             desc = Procedures[procedure]
             cursor.execute(desc)
-            
         logger.info("Procedures created")
+
+        for setting in Globals:
+            desc = Globals[setting]
+            cursor.execute(desc)
+        logger.info("InnoDB settings changed")
 
     except mysql.connector.Error as err:
         logger.error(err.msg)
     
     tmp = demo.ACIDDemo()
     tmp.demo_isolation_deadlock()
+    #demo.atomicityDemo()
     #demo.ACIDDemo.runMenu() # start demo
+    close = input("input to exit")
 
 
 
